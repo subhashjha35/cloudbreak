89cb98cbb03b1f1e731ef659c664b9b838833a49
Jamison Bennett <jamison.bennett@gmail.com>
2020-03-24 11:49:16 -0500

    CB-6248: Add subject alt names to DNS load balanced certificates
    Fix LDAP and FreeIPA x509v3 certifificates to have the subject
    alternative names. This fixes errors with clients that use the DNS
    load balance services.
    
    The LDAP certificate now has ldap.domain entry.
    The FreeIPA API now has freeipa.domain, kdc.domain, and
    kerberos.domain.

--- freeipa/src/main/resources/freeipa-salt/salt/freeipa/scripts/freeipa_install.sh
+++ freeipa/src/main/resources/freeipa-salt/salt/freeipa/scripts/freeipa_install.sh
@@ -5,6 +5,19 @@ set -e
 FQDN=$(hostname -f)
 IPADDR=$(hostname -i)
 
+getCertRequestIdFromDir() {
+  CERT_DIR=$1
+  NICKNAME=$2
+
+  ipa-getcert list -d ${CERT_DIR} -n ${NICKNAME} | grep "Request ID" | cut -d\' -f2
+}
+
+getCertRequestIdFromFile() {
+  FILE=$1
+
+  ipa-getcert list -f ${FILE} | grep "Request ID" | cut -d\' -f2
+}
+
 ipa-server-install \
           --realm $REALM \
           --domain $DOMAIN \
@@ -22,5 +35,32 @@ ipa-server-install \
           --ip-address $IPADDR \
           --auto-forwarders \
           --unattended
+
+# Setup basic CNAME records for pointing to FreeIPA services
+echo "$FPW" | kinit admin
+ipa dnsrecord-add $DOMAIN kdc --cname-rec="ipa-ca.$DOMAIN."
+ipa dnsrecord-add $DOMAIN kerberos --cname-rec="ipa-ca.$DOMAIN."
+ipa dnsrecord-add $DOMAIN ldap --cname-rec="ipa-ca.$DOMAIN."
+ipa dnsrecord-add $DOMAIN freeipa --cname-rec="ipa-ca.$DOMAIN."
+
+LDAP_CERT_REQUEST_ID=`getCertRequestIdFromDir /etc/dirsrv/slapd-${REALM//./-} Server-Cert`
+ipa host-add --force ldap.$DOMAIN
+ipa service-add ldap/ldap.$DOMAIN
+ipa service-add-host ldap/ldap.$DOMAIN --host $FQDN
+ipa-getcert resubmit -D ldap.$DOMAIN -i $LDAP_CERT_REQUEST_ID -w
+
+HTTP_CERT_REQUEST_ID=`getCertRequestIdFromDir /etc/httpd/alias Server-Cert`
+ipa host-add --force freeipa.$DOMAIN
+ipa service-add HTTP/freeipa.$DOMAIN
+ipa service-add-host HTTP/freeipa.$DOMAIN --host $FQDN
+ipa host-add --force kdc.$DOMAIN
+ipa service-add HTTP/kdc.$DOMAIN
+ipa service-add-host HTTP/kdc.$DOMAIN --host $FQDN
+ipa host-add --force kerberos.$DOMAIN
+ipa service-add HTTP/kerberos.$DOMAIN
+ipa service-add-host HTTP/kerberos.$DOMAIN --host $FQDN
+ipa-getcert resubmit -D freeipa.$DOMAIN -D kdc.$DOMAIN -D kerberos.$DOMAIN -i $HTTP_CERT_REQUEST_ID -w
+
+kdestroy
 
 set +e
--- freeipa/src/main/resources/freeipa-salt/salt/freeipa/scripts/freeipa_replica_install.sh
+++ freeipa/src/main/resources/freeipa-salt/salt/freeipa/scripts/freeipa_replica_install.sh
@@ -5,6 +5,19 @@ set -e
 FQDN=$(hostname -f)
 IPADDR=$(hostname -i)
 
+getCertRequestIdFromDir() {
+  CERT_DIR=$1
+  NICKNAME=$2
+
+  ipa-getcert list -d ${CERT_DIR} -n ${NICKNAME} | grep "Request ID" | cut -d\' -f2
+}
+
+getCertRequestIdFromFile() {
+  FILE=$1
+
+  ipa-getcert list -f ${FILE} | grep "Request ID" | cut -d\' -f2
+}
+
 ipa-replica-install \
           --server $FREEIPA_TO_REPLICATE \
           --setup-ca \
@@ -21,5 +34,19 @@ ipa-replica-install \
           --ip-address $IPADDR \
           --auto-forwarders \
           --unattended
+
+echo "$FPW" | kinit admin
+
+LDAP_CERT_REQUEST_ID=`getCertRequestIdFromDir /etc/dirsrv/slapd-${REALM//./-} Server-Cert`
+ipa service-add-host ldap/ldap.$DOMAIN --host $FQDN
+ipa-getcert resubmit -D ldap.$DOMAIN -i $LDAP_CERT_REQUEST_ID -w
+
+HTTP_CERT_REQUEST_ID=`getCertRequestIdFromDir /etc/httpd/alias Server-Cert`
+ipa service-add-host HTTP/freeipa.$DOMAIN --host $FQDN
+ipa service-add-host HTTP/kdc.$DOMAIN --host $FQDN
+ipa service-add-host HTTP/kerberos.$DOMAIN --host $FQDN
+ipa-getcert resubmit -D freeipa.$DOMAIN -D kdc.$DOMAIN -D kerberos.$DOMAIN -i $HTTP_CERT_REQUEST_ID -w
+
+kdestroy
 
 set +e
